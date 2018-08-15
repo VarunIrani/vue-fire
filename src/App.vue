621b192808349b9e908f47b32511826649e16c46
VarunIrani <varunirani0505@gmail.com>
2018-08-15 20:50:32 +0530

    Added Firebase

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
   <div id="app">
-    
+    <div>
+      <label for="name">Name: </label>
+      <input type="text" name="name" v-model="name"/>
+      <button @click="submitName()">ADD</button>
+    </div>
+
+    <div>
+      <ul>
+        <li v-for="personName of names" v-bind:key="personName['.key']">
+          <div v-if="!personName.edit">
+            <p>{{personName.name}}</p>
+            <button @click="removeName(personName['.key'])">REMOVE</button>
+            <button @click="setEditName(personName['.key'])">EDIT</button>
+          </div>
+          <div v-else>
+            <input type="text" name="editName" v-model="personName.name">
+            <button @click="saveEdit(personName)">SAVE</button>
+            <button @click="cancelEdit(personName['.key'])">CANCEL</button>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import { namesRef } from "./firebase";
 export default {
   name: "app",
   data() {
-    return {};
+    return {
+      name: ""
+    };
+  },
+  firebase: {
+    names: namesRef
+  },
+  methods: {
+    submitName() {
+      if (this.name !== "") {
+        namesRef.push({ name: this.name, edit: false });
+        this.name = "";
+      }
+    },
+    removeName(key) {
+      namesRef.child(key).remove();
+    },
+    setEditName(key) {
+      namesRef.child(key).update({ edit: true });
+    },
+    cancelEdit(key) {
+      namesRef.child(key).update({ edit: false });
+    },
+    saveEdit(person) {
+      const key = person[".key"];
+      namesRef.child(key).set({ name: person.name, edit: false });
+    }
   }
 };
 </script>
 
 <style>
+@import url(https://fonts.googleapis.com/css?family=Roboto+Mono);
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Roboto Mono";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 24pt;
+}
+
+input {
+  font-size: 24pt;
+  font-family: "Roboto Mono";
 }
 
 h1,
@@ -34,7 +89,6 @@ ul {
 }
 
 li {
-  display: inline-block;
   margin: 0 10px;
 }
 
@@ -45,5 +99,8 @@ a {
 button {
   background-color: transparent;
   border: 2px solid black;
+  font-size: 24pt;
+  padding: 0 0.5em 0 0.5em;
+  font-family: "Roboto Mono";
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './firebase'
+import VueFire from 'vuefire'
+
+Vue.use(VueFire);
 
 new Vue({
   el: '#app',
